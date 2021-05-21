95cab2b4d1c8507a4e125f189c4a22cbb13d7f9b
sarahartsoe <sarahartsoe@gmail.com>
2021-05-21 10:27:40 -0400

    updated some name field validation.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+// function totalcost() {
+//     let total = 0;
+//     for (i=1; i<total_items; i++) {
+//         itemID = document.getElementById("#days"=i);
+//         if (typeof itemID === 'undefined' || itemID === null) {
+//         } else {
+//             total = total + parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price"));
+//         }
+//     }
+//     document.getElementById("#totalcost").innerHTML = "$" + total;
+// }
+
+// grab the number of days value with document.querySelector
+//start the index at 1 and loop through the end (30). take the amounf of days (innerHTML/innerText?) and times them by 5.
+//output the total into the totalcost div
+
+const form = document.querySelector("#parking-form");
+const nameInput = document.querySelector("#name-field");
+
+let formIsValid;
+
+form.addEventListener('input', event => {
+
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    validateNameInput ();
+    if (formIsValid) {
+        document.querySelector("#name-field").classList.add('input-invalid')
+        document.querySelector('#name-field').appendChild(error).innerHTML = 'This field is required.'
+} else {
+    formIsValid = true
+}
+})
